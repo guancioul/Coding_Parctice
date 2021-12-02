9dde0e4f23c6d83129a8be692217fcf1ff99a105
guancioul <b0543028@ems.niu.edu.tw>
2021-12-02 20:33:28 +0800

    odd even linked list template

---
+++ cpp/leetcode/328_Odd_Even_LInked_List.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) : val(0), next(next) {}
+}
+
+int main(){
+    return 0;
+}
